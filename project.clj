add11ae770375c75d3c87f3e21a8654b2a650296
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-01-11 14:52:45 +0100

    ssl: allow manual specification of certificates

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spootnik/net "0.2.7"
+(defproject spootnik/net "0.2.8"
   :description "the clojure netty companion"
   :url "https://github.com/pyr/net"
   :license {:name "MIT License"}
--- src/net/ssl.clj
+++ src/net/ssl.clj
@@ -2,14 +2,42 @@
   "Clojure glue code to interact with the horrible JVM SSL code"
   (:require [clojure.java.io    :refer [input-stream]])
   (:import java.security.KeyStore
+           java.security.KeyFactory
            java.security.PrivateKey
            java.security.cert.X509Certificate
+           java.security.cert.CertificateFactory
+           java.security.spec.PKCS8EncodedKeySpec
+           java.io.ByteArrayInputStream
+           javax.xml.bind.DatatypeConverter
            io.netty.handler.ssl.SslContextBuilder))
+
+(defn ^X509Certificate s->cert
+  [factory input]
+  (.generateCertificate factory (ByteArrayInputStream. (.getBytes input))))
+
+(defn ^PrivateKey s->pkey
+  "When reading private keys, we unfortunately have to
+  read PKCS8 encoded keys, short of pulling-in bouncy castle :-(
+  Since these keys are usually DER encoded, they're unconvienent to
+  have laying around in strings. We resort to base64 encoded DER here."
+  [factory input]
+  (let [bytes (DatatypeConverter/parseBase64Binary input)
+        kspec (PKCS8EncodedKeySpec. bytes)]
+    (.generatePrivate factory kspec)))
 
 (defn netty-client-context
   "Build an SSL client context for netty"
-  [{:keys [bundle password]}]
+  [{:keys [bundle password cert pkey authority]}]
   (let [builder (SslContextBuilder/forClient)]
+    (when (and cert pkey authority)
+      (let [cert-fact (CertificateFactory/getInstance "X.509")
+            key-fact  (KeyFactory/getInstance "RSA")]
+        (let [cert      (s->cert cert-fact cert)
+              authority (s->cert cert-fact authority)
+              pkey      (s->pkey key-fact pkey)
+              chain     (into-array X509Certificate [cert authority])]
+          (.keyManager builder ^PrivateKey pkey chain)
+          (.trustManager builder chain))))
     (when (and bundle password)
       (let [keystore (KeyStore/getInstance "pkcs12")]
         (with-open [stream (input-stream bundle)]
