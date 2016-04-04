a07cd0822882cd08cde4d4c4c484287e13b678fc
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-04-04 20:38:39 +0200

    project: prepare for next release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spootnik/net "0.2.15"
+(defproject spootnik/net "0.2.16-SNAPSHOT"
   :description "the clojure netty companion"
   :url "https://github.com/pyr/net"
   :license {:name "MIT License"}
