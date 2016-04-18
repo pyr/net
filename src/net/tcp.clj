d0f44161603ea751c6c2b4085f78028f05f084ec
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-04-18 22:26:49 +0200

    net: add a client fn

--- src/net/tcp.clj
+++ src/net/tcp.clj
@@ -2,6 +2,13 @@
   (:require [net.ty.bootstrap :as bootstrap]
             [net.ty.channel   :as channel]))
 
+(defn client
+  ([host port]
+   (client host port {}))
+  ([host port bootstrap-config]
+   (let [bs (bootstrap/bootstrap bootstrap-config)]
+     (-> (bootstrap/remote-address! bs host port)
+         (bootstrap/connect!)))))
 
 (defn server
   [bootstrap-config host port]
