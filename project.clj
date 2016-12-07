88f50608d4eb80710366a2028c09dd6d04e2ae0d
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-12-07 11:49:47 +0100

    project: do not impose log dependencies

--- project.clj
+++ project.clj
@@ -5,5 +5,4 @@
   :dependencies [[org.clojure/clojure       "1.9.0-alpha14"]
                  [org.clojure/core.async    "0.2.395"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [spootnik/unilog           "0.7.15"]
                  [io.netty/netty-all        "4.1.6.Final"]])
