pyr/net

commit ff24b41fd75aa0ce2acc0a247a27f79aca1160ee

fix reflection (#10)

src/net/http/server.clj
@@ -50,10 +50,13 @@
            io.netty.handler.codec.http.LastHttpContent
            io.netty.handler.codec.http.HttpVersion
            io.netty.handler.codec.http.HttpObjectAggregator
+           io.netty.handler.codec.http.HttpObject
            io.netty.handler.codec.http.QueryStringDecoder
+           io.netty.bootstrap.AbstractBootstrap
            io.netty.bootstrap.ServerBootstrap
            io.netty.buffer.Unpooled
            io.netty.buffer.ByteBuf
+           io.netty.channel.ChannelPipeline
            io.netty.buffer.ByteBufAllocator
            io.netty.buffer.UnpooledByteBufAllocator
            java.io.InputStream
@@ -100,7 +103,7 @@
 (defn qs->body-params
   "Extract body parameters from a body when application"
   [{:keys [headers body]}]
-  (when-let [content-type (:content-type headers)]
+  (when-let [^String content-type (:content-type headers)]
     (when (.startsWith content-type "application/x-www-form-urlencoded")
       (->params
        (QueryStringDecoder. (http/bb->string body) false)))))
@@ -138,7 +141,7 @@
 (extend-protocol ChunkEncoder
   (Class/forName "[B")
   (chunk->http-object [chunk]
-    (DefaultHttpContent. (Unpooled/wrappedBuffer chunk)))
+    (DefaultHttpContent. (Unpooled/wrappedBuffer ^"[B" chunk)))
 
   ByteBuffer
   (chunk->http-object [chunk]
@@ -248,10 +251,10 @@
 (defn backpressure-fn
   "Stop automatically reading from the body channel when we are signalled
    for backpressure."
-  [ctx]
+  [^ChannelHandlerContext ctx]
   (fn [enable?]
     (warn "switching backpressure mode to:" enable?)
-    (-> ctx chan/channel .config (.isAutoRead (not enable?)))))
+    (-> ctx chan/channel .config (.setAutoRead (not enable?)))))
 
 (defn close-fn
   "A closure over a context that will close it when called."
@@ -266,7 +269,7 @@
 
 (defmethod write-chunk ::aggregated
   [{:keys [request] :as state} handler ctx msg close?]
-  (buf/augment-buffer (:body request) (.content msg))
+  (buf/augment-buffer (:body request) (.content ^FullHttpRequest msg))
   (when close?
     (-> state
         (update :request assoc-body-params)
@@ -279,7 +282,7 @@
     (a/close! (:body request))))
 
 
-(defn netty-handler
+(defn ^ChannelHandler netty-handler
   "This is a stateful, per HTTP session adapter which wraps the user
    supplied function.
    We can use volatiles for keeping track of state due to the thread-safe
@@ -306,7 +309,7 @@
                (.write ctx (DefaultFullHttpResponse. HttpVersion/HTTP_1_1
                                                      HttpResponseStatus/CONTINUE)))
              (vswap! state assoc
-                     :version (.getProtocolVersion msg)
+                     :version (.getProtocolVersion ^HttpRequest msg)
                      :request (->request msg))
              (let [length (request-length (:request @state))]
                (if (or (nil? length) (> length agg-length))
@@ -330,7 +333,7 @@
              (error "unhandled message chunk on body channel")
              (throw (IllegalArgumentException. "unhandled message chunk on body channel")))))))))
 
-(defn body-decoder
+(defn ^io.netty.handler.codec.MessageToMessageDecoder body-decoder
   "To simplify the life of downstream consumers, we
    create fixed-size chunks. For example, for an input
    payload of 42M and a chunk size of 16M, the handler
@@ -341,11 +344,11 @@
     (proxy [io.netty.handler.codec.MessageToMessageDecoder] []
       (isSharable []
         false)
-      (decode [ctx msg out]
+      (decode [^ChannelHandlerContext ctx msg ^java.util.List out]
         (try
           (cond
             (instance? HttpRequest msg)
-            (.add out msg)
+            (.add out ^HttpRequest msg)
 
             (buf/last-http-content? msg)
             (doseq [chunk (buf/release-contents max-size content msg)]
@@ -369,14 +372,14 @@
             codec        (HttpServerCodec. 4096 8192 (int chunk-size))
             aggregator   (body-decoder chunk-size)
             handler      (netty-handler ring-handler handler-opts)
-            pipeline     (.pipeline channel)]
-        (.addLast pipeline "codec"      codec)
+            pipeline     (.pipeline ^io.netty.channel.Channel channel)]
+        (.addLast pipeline "codec"       codec)
         (.addLast pipeline "aggregator" aggregator)
         (.addLast pipeline "handler"    handler)))))
 
 (defn set-so-backlog!
   "Adjust Bootstrap socket backlog"
-  [bootstrap {:keys [so-backlog]}]
+  [^AbstractBootstrap bootstrap {:keys [so-backlog]}]
   (.option bootstrap ChannelOption/SO_BACKLOG (int (or so-backlog 1024))))
 
 (defn get-host-port

src/net/ty/channel.clj
@@ -4,61 +4,65 @@
   (:import io.netty.channel.ChannelFuture
            io.netty.channel.ChannelFutureListener
            io.netty.channel.Channel
+           io.netty.channel.ChannelHandlerContext
            io.netty.channel.group.ChannelGroup
            io.netty.channel.group.DefaultChannelGroup
            io.netty.util.concurrent.GlobalEventExecutor))
 
 (defn ^Channel channel
   "Extract the channel from a channel holder"
-  [channel-holder]
+  [^ChannelHandlerContext channel-holder]
   (.channel channel-holder))
 
 (defn await
   "Wait on a channel"
-  [^Channel channel]
+  [^ChannelFuture channel]
   (.await channel))
 
 (defn write!
   "write a payload to a channel"
-  [channel msg]
+  [^Channel channel msg]
   (.write channel msg))
 
 (defn flush!
   "flush a channel"
-  [channel]
+  [^Channel channel]
   (.flush channel))
 
 (defn write-and-flush!
   "write a payload, then flush a channel"
-  [channel msg]
+  [^Channel channel msg]
   (.writeAndFlush channel msg))
 
 (defn channel-group
   "Create a named channel group"
   [name]
-  (DefaultChannelGroup. name GlobalEventExecutor/INSTANCE))
+  (DefaultChannelGroup. ^String name
+                        GlobalEventExecutor/INSTANCE))
 
 (defn add-to-group
   "Add a channel to a channel group"
-  [group chan]
+  [^ChannelGroup group
+   ^Channel chan]
   (.add group chan))
 
 (defn remove-from-group
   "Remove a channel from a channel group"
-  [group chan]
+  [^ChannelGroup group
+   ^Channel chan]
   (.remove group chan))
 
 (defn close!
   "Close a channel"
-  [chan]
+  [^Channel chan]
   (.close chan))
 
 (defn sync-uninterruptibly!
   "Sync a channel, without interruptions"
-  [chan]
+  [^ChannelFuture chan]
   (.syncUninterruptibly chan))
 
-(defn close-future
+(defn ^ChannelFuture close-future
   "Get the close future for a channel"
-  [chan]
+  [^Channel chan]
   (.closeFuture chan))

project.clj
@@ -23,4 +23,5 @@
   :dependencies [[org.clojure/clojure       "1.9.0-alpha14"]
                  [org.clojure/core.async    "0.2.395"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [io.netty/netty-all        "4.1.7.Final"]])
+                 [io.netty/netty-all        "4.1.7.Final"]]
+  :global-vars {*warn-on-reflection* true})

src/net/http.clj
@@ -51,9 +51,8 @@
   []
   (Epoll/isAvailable))
 
-(defn bb->string
-  "Convert a ByteBuf to a UTF-8 String."
-  [bb]
+(defn ^String bb->string  "Convert a ByteBuf to a UTF-8 String."
+  [^ByteBuf bb]
   (.toString bb (Charset/forName "UTF-8")))
 
 (def method->data
@@ -95,12 +94,12 @@
   "Add log hander to a bootstrap"
   [^AbstractBootstrap bootstrap {:keys [logging]}]
   (let [handler (when-let [level (some-> logging keyword (get log-levels))]
-                  (LoggingHandler. level))]
+                  (LoggingHandler. ^LogLevel level))]
     (cond-> bootstrap  handler (.handler handler))))
 
 (defn set-optimal-server-channel!
   "Add optimal channel to a server bootstrap"
-  [bs disable-epoll?]
+  [^AbstractBootstrap bs disable-epoll?]
   (.channel bs (if (and (not disable-epoll?) (epoll?))
                  EpollServerSocketChannel NioServerSocketChannel))
   bs)

src/net/http/client.clj
@@ -6,6 +6,7 @@
             [clojure.spec       :as s]
             [clojure.core.async :as async])
   (:import io.netty.bootstrap.Bootstrap
+           io.netty.buffer.ByteBuf
            io.netty.channel.Channel
            io.netty.channel.ChannelHandlerContext
            io.netty.channel.ChannelHandlerAdapter
@@ -26,6 +27,7 @@
            io.netty.handler.logging.LogLevel
            io.netty.handler.codec.http.HttpClientCodec
            io.netty.handler.codec.http.HttpRequest
+           io.netty.handler.codec.http.DefaultFullHttpRequest
            io.netty.handler.codec.http.HttpMethod
            io.netty.handler.codec.http.HttpHeaders
            io.netty.handler.codec.http.HttpResponseStatus
@@ -160,7 +162,7 @@
 (defn auth->headers
   "If basic auth is present as a map within a request, add the
    corresponding header."
-  [headers {:keys [user password]}]
+  [^HttpHeaders headers {:keys [user password]}]
   (when (and user password)
     (.set headers "Authorization"
           (format "Basic %s" (b64/s->b64 (str user ":" password)))))
@@ -168,15 +170,18 @@
 
 (defn data->body
   "Add body to a request"
-  [request method body]
+  [^DefaultFullHttpRequest request
+   method
+   ^String body]
   (when body
-    (let [headers (.headers request)
-          bytes   (cond
-                    (string? body) (.getBytes body)
-                    :else          (throw (ex-info "wrong body type" {})))]
-      (-> request .content .clear (.writeBytes bytes))
+    (let [^HttpHeaders headers (.headers request)
+          ^"[B" bytes   (cond
+                          (string? body) (.getBytes ^String body)
+                          :else          (throw (ex-info "wrong body type" {})))]
+      (let [^ByteBuf content (.content request)]
+        (-> content .clear (.writeBytes bytes)))
       (when-not (.get headers "Content-Length")
-        (.set headers "Content-Length" (count body))))))
+        (.set headers "Content-Length" (str (count body)))))))
 
 (defn ^URI data->uri
   "Produce a valid URI from arguments found in a request map"

src/net/ssl.clj
@@ -56,12 +56,13 @@
   [input]
   (if (instance? (Class/forName "[B") input)
     input
-    (.getBytes (cert-string input))))
+    (.getBytes ^String (cert-string input))))
 
 (defn ^X509Certificate s->cert
   "Generate an X509 from a given source."
   [factory input]
-  (.generateCertificate factory (ByteArrayInputStream. (cert-bytes input))))
+  (.generateCertificate ^CertificateFactory factory
+                        (ByteArrayInputStream. (cert-bytes input))))
 
 (defn ^PrivateKey s->pkey
   "When reading private keys, we unfortunately have to
@@ -142,7 +143,7 @@
    context, a host, and a port which will add a handler to the context."
   ([^SslContext ctx host port]
    (fn ^ChannelHandler make-handler []
-     (.newHandler ctx (.alloc *channel*) host port)))
+     (.newHandler ctx (.alloc ^Channel *channel*) host port)))
   ([^SslContext ctx]
    (fn ^ChannelHandler make-handler []
-     (.newHandler ctx (.alloc *channel*)))))
+     (.newHandler ctx (.alloc ^Channel *channel*)))))

src/net/ty/bootstrap.clj
@@ -19,7 +19,8 @@
            io.netty.channel.nio.NioEventLoopGroup
            io.netty.channel.socket.nio.NioServerSocketChannel
            io.netty.channel.socket.nio.NioSocketChannel
-           io.netty.channel.socket.nio.NioDatagramChannel))
+           io.netty.channel.socket.nio.NioDatagramChannel
+           java.net.InetAddress))
 
 (def channel-options
   "Valid options for bootstraps"
@@ -57,7 +58,7 @@
 (def nio-socket-channel        NioSocketChannel)
 (def nio-datagram-channel      NioDatagramChannel)
 
-(defn nio-event-loop-group
+(defn ^NioEventLoopGroup nio-event-loop-group
   "Yield a new NioEventLoopGroup"
   []
   (NioEventLoopGroup.))
@@ -81,7 +82,7 @@
     (.childHandler bs (:handler config))
     (.validate bs)))
 
-(defn ^Bootstrap bootstrap
+(defn ^AbstractBootstrap bootstrap
   "Build a client bootstrap from a configuration map"
   [config]
   (when-not (s/valid? ::bootstrap-schema config)
@@ -94,7 +95,7 @@
       (.option bs copt (if (number? v) (int v) v)))
     (doseq [[k v] (:attrs config)]
       (.attr bs (AttributeKey/valueOf (name k)) v))
-    (when-let [[host port] (:remote-address config)]
+    (when-let [[^InetAddress host port] (:remote-address config)]
       (.remoteAddress bs host (int port)))
     (.handler bs (:handler config))
     (.validate bs)))
@@ -102,7 +103,7 @@
 (defn sync!
   "Synchronize bootstrap"
   [^AbstractBootstrap bs]
-  (.sync bs))
+  (.sync bs)) ;; ?? bug?
 
 (defn bind!
   "Bind bootstrap to a host and port"
@@ -112,42 +113,44 @@
 (defn remote-address!
   "Set remote address for a client bootstrap, allows host and port
    to be provided as a SocketAddress"
-  ([bs ^SocketAddress sa]
+  ([^Bootstrap bs ^SocketAddress sa]
    (.remoteAddress bs sa))
-  ([bs host ^Long port]
+  ([^Bootstrap bs
+    ^InetAddress host
+    ^Long port]
    (.remoteAddress bs host (int port))))
 
 (defn connect!
   "Attempt connection of a bootstrap. Accepts as pre-configured bootstrap,
    and optionally a SocketAddressor Host and Port."
-  ([bs]
+  ([^Bootstrap bs]
    (.connect bs))
-  ([bs ^SocketAddress sa]
+  ([^Bootstrap bs ^SocketAddress sa]
    (.connect bs sa))
-  ([bs x y]
-   (.connect bs x y)))
+  ([^Bootstrap bs ^InetAddress x y]
+   (.connect bs x (int y))))
 
 (defn local-address!
   "Sets the bootstrap's local address. Accepts either a SocketAddress or
    Host and Port."
-  ([bs x]
-   (.localAddress bs x))
-  ([bs x y]
-   (.localAddress bs x y)))
+  ([^AbstractBootstrap bs x]
+   (.localAddress bs (int x)))
+  ([^AbstractBootstrap bs ^InetAddress x y]
+   (.localAddress bs x (int y))))
 
 (defn validate!
   "Validate that a bootstrap has correct parameters."
-  ([bs]
+  ([^AbstractBootstrap bs]
    (.validate bs)))
 
 (defn set-group!
   "Set the group on top of which channels will be created and then handled."
-  [bs group]
+  [^AbstractBootstrap bs group]
   (.group bs group))
 
 (defn shutdown-gracefully!
   "Gracefully shut down a group"
-  [group]
+  [^EventLoopGroup group]
   (.shutdownGracefully group))
 
 (defn shutdown-fn
@@ -159,7 +162,8 @@
 
 (defn set-child-handler!
   "A server bootstrap has a child handler, this methods helps set it"
-  [bootstrap handler]
+  [^ServerBootstrap bootstrap
+   ^ChannelHandler handler]
   (.childHandler bootstrap handler))
 
 ;; Specs

src/net/ty/buffer.clj
@@ -4,12 +4,14 @@
    a specific size has been reached."
   (:require [clojure.tools.logging :refer [debug]])
   (:import io.netty.buffer.Unpooled
+           io.netty.buffer.ByteBuf
            io.netty.handler.codec.http.DefaultHttpResponse
            io.netty.handler.codec.http.DefaultHttpContent
            io.netty.handler.codec.http.DefaultLastHttpContent
            io.netty.handler.codec.http.HttpRequest
            io.netty.handler.codec.http.HttpContent
-           io.netty.handler.codec.http.LastHttpContent))
+           io.netty.handler.codec.http.LastHttpContent
+           java.nio.ByteBuffer))
 
 (defn buffer-holder
   "Create a buffer holder (internally a volatile)."
@@ -18,11 +20,11 @@
 
 (defn augment-buffer
   "Add bytes to a buffer"
-  ([dst src]
+  ([^ByteBuf dst ^ByteBuf src]
    (if (pos? (.capacity src))
      (.writeBytes dst src)
      dst))
-  ([dst src len]
+  ([^ByteBuf dst ^ByteBuf src len]
    (.writeBytes dst src (int len))))
 
 (defn new-buffer
@@ -35,10 +37,10 @@
 (defn update-content
   "Add bytes to a buffer, outputs HttpContent instances containing
    the expected size, or nothing."
-  [max-size content msg]
+  [max-size ^HttpContent content ^HttpContent msg]
   (locking content
-    (let [buf    @content
-          msg-bb (.content msg)
+    (let [buf    ^ByteBuf @content
+          msg-bb ^ByteBuffer (.content  msg)
           msg-sz (.capacity msg-bb)
           buf-sz (if buf (.writerIndex buf) 0)
           new-sz (+ buf-sz msg-sz)
@@ -47,7 +49,7 @@
         (and (nil? buf) (= max-size msg-sz))
         (do
           (debug "nil buf, msg size matches max-chunk, forwarding")
-          (DefaultHttpContent. (Unpooled/copiedBuffer msg-bb)))
+          (DefaultHttpContent. ^ByteBuf (Unpooled/copiedBuffer msg-bb)))
 
         (nil? buf)
         (do
@@ -80,12 +82,14 @@
 (defn release-contents
   "Add bytes to a buffer, and then flush all output. Yields a vector
   of HttpContent instances."
-  [max-size content msg]
+  [max-size
+   ^HttpContent content
+   ^HttpContent msg]
   (locking content
     (let [buf    @content
           msg-bb (.content msg)
           msg-sz (.capacity msg-bb)
-          buf-sz (if buf (.writerIndex buf) 0)
+          buf-sz (if buf (.writerIndex ^ByteBuf buf) 0)
           new-sz (+ buf-sz msg-sz)
           pad-sz (- max-size buf-sz)]
       (cond
@@ -110,5 +114,4 @@
 (defn last-http-content?
   "Are we dealing with an instance of LastHttpContent"
   [msg]
-  (let [has-interface? (-> msg .getClass supers set)]
-    (has-interface? LastHttpContent)))
+  (instance? LastHttpContent msg))

commit 26d6e76bde1a6c429b86303900c4e85bb3560866

fix bugs introduced by ff24b41fd75aa0ce2acc0a247a27f79aca1160ee

src/net/http/server.clj
@@ -161,7 +161,7 @@
 
   String
   (chunk->http-object [chunk]
-    (Unpooled/wrappedBuffer (.getBytes chunk "UTF8")))
+    (DefaultHttpContent. (Unpooled/wrappedBuffer (.getBytes chunk "UTF8"))))
 
   HttpContent
   (chunk->http-object [chunk] chunk))
@@ -251,7 +251,7 @@
 (defn backpressure-fn
   "Stop automatically reading from the body channel when we are signalled
    for backpressure."
-  [^ChannelHandlerContext ctx]
+  [ctx]
   (fn [enable?]
     (warn "switching backpressure mode to:" enable?)
     (-> ctx chan/channel .config (.setAutoRead (not enable?)))))

src/net/ty/channel.clj
@@ -7,32 +7,86 @@
            io.netty.channel.ChannelHandlerContext
            io.netty.channel.group.ChannelGroup
            io.netty.channel.group.DefaultChannelGroup
-           io.netty.util.concurrent.GlobalEventExecutor))
-
-(defn ^Channel channel
-  "Extract the channel from a channel holder"
-  [^ChannelHandlerContext channel-holder]
-  (.channel channel-holder))
+           io.netty.util.concurrent.GlobalEventExecutor
+           io.netty.bootstrap.AbstractBootstrap$PendingRegistrationPromise))
 
 (defn await
   "Wait on a channel"
   [^ChannelFuture channel]
   (.await channel))
 
-(defn write!
-  "write a payload to a channel"
-  [^Channel channel msg]
-  (.write channel msg))
+(defn ^Channel channel
+  "Extract the channel from a channel holder"
+  [^AbstractBootstrap$PendingRegistrationPromise channel-holder]
+  (.channel channel-holder))
 
-(defn flush!
-  "flush a channel"
-  [^Channel channel]
-  (.flush channel))
+(defprotocol ChannelResource
+  (write! [this msg] "write a payload to resource")
+  (write-and-flush! [this msg] "write a payload, then flush a resource")
+  (flush! [this] "Request to flush all pending messages")
+  (close! [this] "Request to close resource")
+  (disconnect! [this])
+  (deregister! [this]))
 
-(defn write-and-flush!
-  "write a payload, then flush a channel"
-  [^Channel channel msg]
-  (.writeAndFlush channel msg))
+(extend-type ChannelHandlerContext
+  ChannelResource
+  (write! [ch msg]
+    (.write ch msg))
+
+  (write-and-flush! [ch msg]
+    (.writeAndFlush ch msg))
+
+  (flush! [ch]
+    (.flush ch))
+
+  (close! [ch]
+    (.close ch))
+
+  (disconnect! [ch]
+    (.disconnect ch))
+
+  (deregister! [ch]
+    (.deregister ch)))
+
+(extend-type Channel
+  ChannelResource
+  (write! [ch msg]
+    (.write ch msg))
+
+  (write-and-flush! [ch msg]
+    (.writeAndFlush ch msg))
+
+  (flush! [ch]
+    (.flush ch))
+
+  (close! [ch]
+    (.close ch))
+
+  (disconnect! [ch]
+    (.disconnect ch))
+
+  (deregister! [ch]
+    (.deregister ch)))
+
+(extend-type ChannelGroup
+  ChannelResource
+  (write! [ch-g msg]
+    (.write ch-g msg))
+
+  (write-and-flush! [ch-g msg]
+    (.writeAndFlush ch-g msg))
+
+  (flush! [ch-g]
+    (.flush ch-g))
+
+  (close! [ch]
+    (.close ch))
+
+  (disconnect! [ch]
+    (.disconnect ch))
+
+  (deregister! [ch]
+    (.deregister ch)))
 
 (defn channel-group
   "Create a named channel group"
@@ -51,11 +105,6 @@
   [^ChannelGroup group
    ^Channel chan]
   (.remove group chan))
-
-(defn close!
-  "Close a channel"
-  [^Channel chan]
-  (.close chan))
 
 (defn sync-uninterruptibly!
   "Sync a channel, without interruptions"