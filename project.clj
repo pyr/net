2c929ff9569cac18f80f84f71482c9f1ca0c0271
Pierre-Yves Ritschard <pyr@spootnik.org>
2018-01-18 22:13:12 +0100

    project: this justifies a bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spootnik/net "0.3.3-beta19"
+(defproject spootnik/net "0.3.3-beta20"
   :description "A clojure netty companion"
   :url "https://github.com/pyr/net"
   :license {:name "MIT License"
