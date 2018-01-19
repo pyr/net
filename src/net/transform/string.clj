b68e73ef1e98dd1e36d151f43b44fb9573511494
Pierre-Yves Ritschard <pyr@spootnik.org>
2018-01-19 23:30:19 +0100

    transform: use in-house buf release method

--- src/net/transform/string.clj
+++ src/net/transform/string.clj
@@ -1,14 +1,8 @@
 (ns net.transform.string
   (:require [net.ty.buffer :as buf]))
 
-(defn released-string
-  [buf]
-  (let [s (buf/to-string buf)]
-    (buf/release buf)
-    s))
-
 (def transform
   "A naive transform definition"
   {:reducer (completing str)
-   :xf      (map released-string)
+   :xf      (map #(buf/to-string % true))
    :init    ""})
