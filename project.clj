6e20a09222104fb10f764cbc8a366bfb18ee941a
Pierre-Yves Ritschard <pyr@spootnik.org>
2018-02-08 16:24:43 +0100

    uri: support supplying parameters as a map (#32)
    * uri: support supplying parameters as a map
    
    * prepare for next iteration
    
    * boolean is hard

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spootnik/net "0.3.3-beta26"
+(defproject spootnik/net "0.3.3-beta27-SNAPSHOT"
   :description "A clojure netty companion"
   :url "https://github.com/pyr/net"
   :license {:name "MIT License"
