f17e61318628541468f70bf84f54cc7512532c40
Yoan Blanc <yoan.blanc@exoscale.ch>
2019-03-04 08:14:54 +0100

    hex: fix exception...
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- src/net/codec/hex.clj
+++ src/net/codec/hex.clj
@@ -8,6 +8,6 @@
       (let [ba (.getBytes s)]
         (throw (ex-info "cannot parse input string as hex value"
                         {:sample  (vec (take 5 (seq ba)))
-                         :zeroes (reduce + 0 (remove zero?  ba))
-                         :str (reduce str (map char (remove zero? ba)))
-                         :size    (count (.getBytes s))}))))))
+                         :zeroes  (reduce + 0 (remove zero?  ba))
+                         :str     s
+                         :size    (count ba)}))))))
