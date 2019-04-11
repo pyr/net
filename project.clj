8c7e41d82e4cf889631202a51dabad22c3a1baf7
Yoan Blanc <yoan@dosimple.ch>
2019-04-11 13:25:44 +0200

    deps: bump all but netty 5.x (#54)
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- project.clj
+++ project.clj
@@ -17,14 +17,14 @@
   :profiles {:dev {:resource-paths ["test/resources"]
                    :source-paths   ["examples"]
                    :main           webfile.server
-                   :dependencies   [[org.slf4j/slf4j-api        "1.7.25"]
-                                    [org.slf4j/slf4j-log4j12    "1.7.25"]
+                   :dependencies   [[org.slf4j/slf4j-api        "1.7.26"]
+                                    [org.slf4j/slf4j-log4j12    "1.7.26"]
                                     [org.clojure/tools.logging  "0.4.1"]
-                                    [com.stuartsierra/component "0.3.2"]]
+                                    [com.stuartsierra/component "0.4.0"]]
                    :jvm-opts       ["-Dio.netty.leakDetection.level=paranoid"]}}
-  :dependencies [[org.clojure/clojure                      "1.9.0"]
-                 [org.clojure/core.async                   "0.4.474"]
-                 [io.netty/netty-all                       "4.1.25.Final"]
-                 [io.netty/netty-tcnative                  "2.0.7.Final"]
-                 [io.netty/netty-tcnative-boringssl-static "2.0.7.Final"]]
+  :dependencies [[org.clojure/clojure                      "1.10.0"]
+                 [org.clojure/core.async                   "0.4.490"]
+                 [io.netty/netty-all                       "4.1.33.Final"]
+                 [io.netty/netty-tcnative                  "2.0.22.Final"]
+                 [io.netty/netty-tcnative-boringssl-static "2.0.22.Final"]]
   :global-vars {*warn-on-reflection* true})
